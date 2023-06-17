e56143d5cabe124e84627f22567e733f1dc1bbd0
boris <boris.lok@cienet.com>
2023-06-17 11:22:50 +0800

    [Refactoring] change some small issues.

--- adder2.go
+++ adder2.go
@@ -15,18 +15,18 @@ const (
 )
 
 type Number struct {
-	Sign string
+	Sign  string
 	Value []int
 }
 
 func Add(val1 []int, val2 []int) string {
 	temp := 0
-	res := make([]string, len(val1) + 1)
-	for i := len(val1)-1; i >= 0; i -- {
+	res := make([]string, len(val1)+1)
+	for i := len(val1) - 1; i >= 0; i-- {
 		digit := val1[i] + val2[i] + temp
 		temp = digit / 10
 		digit = digit % 10
-		res[i + 1] = strconv.Itoa(digit)
+		res[i+1] = strconv.Itoa(digit)
 	}
 	if temp == 1 {
 		res[0] = "1"
@@ -40,7 +40,7 @@ func Subtract(val1 []int, val2 []int) string {
 	res := make([]int, len(val1))
 	for i := range val1 {
 		if val1[i] < val2[i] {
-			res[i - 1] = res[i - 1] - 1
+			res[i-1] = res[i-1] - 1
 			res[i] = 10 + val1[i] - val2[i]
 		} else {
 			res[i] = val1[i] - val2[i]
@@ -56,6 +56,9 @@ func Subtract(val1 []int, val2 []int) string {
 }
 
 func PaddingZero(val []int, digit int) []int {
+	if len(val) >= digit {
+		return val
+	}
 	zeros := make([]int, digit-len(val))
 	return append(zeros, val...)
 }
@@ -75,18 +78,18 @@ func CompareValue(val1 []int, val2 []int) Outcome {
 // Remove the first few zero from string except only one zero
 func RemoveZero(str string) string {
 	idx := 0
-	for ; idx < len(str) && string(str[idx]) == "0"; idx++ {}
+	for ; idx < len(str) && string(str[idx]) == "0"; idx++ {
+	}
 
 	return str[idx:]
 }
 
 // Get sign and value
 func ExtractSignAndNum(str string) (string, string) {
-	switch string(str[0]) {
-	case "-":
-		return "-", str[1:]
-	case "+":
-		return "+", str[1:]
+  firstChar := string(str[0])
+	switch firstChar {
+	case "-", "+":
+		return firstChar, str[1:]
 	default:
 		return "+", str[:]
 	}
@@ -113,27 +116,26 @@ func Adder(num1 string, num2 string) string {
 
 	// pad zero depends on the larger num
 	// Ex: [2]int{1,2}, [3]int{1,2,3} -> [3]{0,1,2}, [3]int{1,2,3}
-	if len(number1.Value) > len(number2.Value) {
-		number2.Value = PaddingZero(number2.Value, len(number1.Value))
-	} else if len(number1.Value) < len(number2.Value) {
-		number1.Value = PaddingZero(number1.Value, len(number2.Value))
-	}
+	number2.Value = PaddingZero(number2.Value, len(number1.Value))
+	number1.Value = PaddingZero(number1.Value, len(number2.Value))
 
 	// If signs are the same, do add() and add sign on the result
 	if number1.Sign == number2.Sign {
-		return strings.TrimPrefix(number1.Sign + RemoveZero(Add(number1.Value, number2.Value)), "+")
+		return strings.TrimPrefix(number1.Sign+RemoveZero(Add(number1.Value, number2.Value)), "+")
 	}
 
 	// If signs are different, use large value to subtract small value
 	// If value of negative input is larger, add "-" to the result
 	// If values are the same, return 0
-	if CompareValue(number1.Value, number2.Value) == Larger {
+	switch CompareValue(number1.Value, number2.Value) {
+	case Larger:
 		res := RemoveZero(Subtract(number1.Value, number2.Value))
-		return strings.TrimPrefix(number1.Sign + res, "+")
-	} else if CompareValue(number1.Value, number2.Value) == Less {
+		return strings.TrimPrefix(number1.Sign+res, "+")
+	case Less:
 		res := RemoveZero(Subtract(number2.Value, number1.Value))
-		return strings.TrimPrefix(number2.Sign + res, "+")
-	} else {
+		return strings.TrimPrefix(number2.Sign+res, "+")
+	default:
 		return "0"
 	}
 }
+
